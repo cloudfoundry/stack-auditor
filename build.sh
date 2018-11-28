3999f16e8f56a93e622a1965c30bda3c2bf4a28e
Zander Mackie <amackie@pivotal.io>
2018-11-28 14:02:18 -0500

    Build script
    [#161733623]
    
    Co-authored-by: Guillermo Kardolus <gkardolus@pivotal.io>

---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+mkdir -p build
+
+go build -ldflags="-s -w" -o build/stack-auditor.darwin github.com/cloudfoundry/stack-auditor
+GOOS=linux go build -ldflags="-s -w" -o build/stack-auditor.linux github.com/cloudfoundry/stack-auditor
+GOOS=windows go build -ldflags="-s -w" -o build/stack-auditor.exe github.com/cloudfoundry/stack-auditor
